fc269fdbc365add22adcb21dfae884729df13151
Peterseo9503 <tjehdgus9503@gmail.com>
2025-01-07 09:50:40 +0900

    20250107

---
+++ javascript&node/20250106/number.js
@@ -0,0 +1,11 @@
+console.log(9999999999999999);
+
+console.log(36.724e-4);
+
+function logBase(base, x) {
+    return Math.log(x) / Math.log(base);
+}
+
+console.log(Math.log(100))
+
+console.log( "1234".substring(0,3) )
